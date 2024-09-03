5cbac3e14164fbc7b95accecffa845ca666c02bb
01igarchyOfGOATs <hyang01@bvsd.org>
2024-09-03 17:11:18 +0000

    conversion with scanner

--- Conversions.java
+++ Conversions.java
@@ -2,39 +2,71 @@ import java.util.Scanner;
 /**
  * This application converts between various units of measurement.
  *
- * @author Tim Gesell
- * @version 8/21/2022
+ * @author Hubert Yang
+ * @version 8/21/2024
  */
 public class Conversions
 {
     public static void main(String[] args)
     {
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("1. Celsius to Fahrenheit");
-        System.out.println("2. Fahrenheit to Celsius");
-        System.out.println("3. Feet to Meters");
-        System.out.println("4. Meters to Feet");
-        System.out.println("5. Ounces to Milliliters");
-        System.out.println("6. Milliliters to Ounces");
-        //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
-        //You can add even more if you would like.
-
-        int selection = keyboard.nextInt();
-        keyboard.nextLine();  //go past the endline and be ready for more input
-
-        if (selection == 1)
-        {
-            System.out.println("Enter Celsius: ");
-            double celsius = keyboard.nextDouble();
+        try (Scanner keyboard = new Scanner(System.in)) {
+            System.out.println("enter your input for celsius");
+            double celsius = keyboard.nextInt();
+            keyboard.nextLine();  //go past the endline and be ready for more input
+            System.out.println(celsius*1.8 + 32);
+            System.out.println("this is the conversion from celsius to fahrenheit");
+            System.out.println("enter your input for fahrenheit");
+            double fahrenheit = keyboard.nextInt();
             keyboard.nextLine();
-            double fahrenheit = celsius * (9.0 / 5.0) + 32;
-            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+            System.out.println((fahrenheit-32)/1.8);
+            System.out.println("this is the conversion from fahrenheit to celsius");
+            System.out.println("enter your input for feet");
+            double feet = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(feet*3.281);
+            System.out.println("this is the approximate conversion from feet to meters");
+            System.out.println("enter your input for meters");
+            double meters = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(meters/3.281);
+            System.out.println("this is the approximate conversion from meters to feet");
+            System.out.println("enter your input for ounces");
+            double ounces = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(ounces*29.574);
+            System.out.println("this is the approximate conversion from ounces to milliliters");
+            System.out.println("enter your input for milliliters");
+            double milliliters = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(milliliters/29.574);
+            System.out.println("this is the approximate conversion from milliliters to ounces");
+            System.out.println("enter your input for inches");
+            double inches = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(inches*12);
+            System.out.println("this is the conversion from inches to feet");
+            System.out.println("enter your input for feet");
+            double miles = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(miles*5280);
+            System.out.println("this is the conversion from miles to feet");
+            System.out.println("enter your input for liters");
+            double liters = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(liters*1000);
+            System.out.println("this is the conversion from liters to milliliters");
+            System.out.println("enter your input for kilograms");
+            double kilograms = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println(kilograms*1000);
+            System.out.println("this is the conversion from kilograms to grams");
+
+            //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
+            //You can add even more if you would like.
+
+
+
+
         }
-        if (selection == 2)
-        {
-            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
-        }
-        //add more if statements here.
     }
 }
